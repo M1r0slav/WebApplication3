ae532eedcab5089952f2842acf584baf20992b4d
M1r0slav <72496884+M1r0slav@users.noreply.github.com>
2021-11-26 12:11:32 +0200

    Initial commit

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ArticlesController:Controller
+    {
+        public ActionResult ById(string name)
+        {
+            var viewModel = new DataViewModels();
+            viewModel.ParameterValue = name;
+            return this.View(viewModel);
+        }
+    }
+}
